8714811ba7680fc7edab561fe13781e849ed4830
Batuhan Mutlu <batuhan.mutlu.0566@gmail.com>
2022-01-14 16:30:09 +0300

    Binary Search Tree with Python

--- Graph/BinarySearchTree.py
+++ Graph/BinarySearchTree.py
@@ -47,6 +47,52 @@ def postorderTraversal(root):
     print("%d " % root.val)
 
 
+def find(root, key):
+    if root is None or key == root.val:
+        return root
+    else:
+        if key < root.val:
+            return find(root.left, key)
+        elif key > root.val:
+            return find(root.right, key)
+
+
+def findSmallest(root):
+    if root is None or root.left is None:
+        return root
+    else:
+        return findSmallest(root.left)
+
+
+def findLargest(root):
+    if root is None or root.right is None:
+        return root
+    else:
+        return findLargest(root.right)
+
+
+def delete(root, key):
+    temp = Node(None)
+    if root is None:
+        return root
+    elif key < root.val:
+        root.left = delete(root.left, key)
+    elif key > root.val:
+        root.right = delete(root.right, key)
+    elif root.left is not None and root.right is not None:
+        temp = findLargest(root.left, key)
+        root.val = temp.val
+        root.left = delete(root.left, temp.val)
+    else:
+        if root.left is None and root.right is None:
+            root = None
+        elif root.left is not None:
+            root = root.left
+        else:
+            root = root.right
+    return root
+
+
 root = Node(85)
 insert(root, Node(4))
 insert(root, Node(36))
@@ -55,6 +101,23 @@ insert(root, Node(57))
 insert(root, Node(52))
 insert(root, Node(91))
 insert(root, Node(87))
+
+print("Preorder Traversal")
+preorderTraversal(root)
+print("Inorder Traversal")
+inorderTraversal(root)
+print("Postorder Traversal")
+postorderTraversal(root)
+
+print(find(root, 57).val)
+print("Smallest Element : ", findSmallest(root).val)
+print("Largest Element : ", findLargest(root).val)
+
+root = delete(root, findLargest(root).val)
+root = delete(root, findSmallest(root).val)
+
+print("Smallest Element : ", findSmallest(root).val)
+print("Largest Element : ", findLargest(root).val)
 
 print("Preorder Traversal")
 preorderTraversal(root)
